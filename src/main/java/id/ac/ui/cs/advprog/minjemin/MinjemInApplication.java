2f5c24d763e72ffc8a1023aeee481a659b880f22
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2022-04-17 21:48:50 +0800

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/minjemin/MinjemInApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.minjemin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinjemInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinjemInApplication.class, args);
+    }
+
+}
