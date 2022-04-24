35cb5f89ccc5ee14ecdd6d14a6ab0516ca14b7a3
Veloraine <bivanderr@gmail.com>
2022-04-24 12:08:27 +0700

    fix script for tests

--- src/test/java/id/ac/ui/cs/advprog/minjemin/MinjemInApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/minjemin/MinjemInApplicationTests.java
@@ -1,13 +1,16 @@
 package id.ac.ui.cs.advprog.minjemin;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class MinjemInApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//        assertTrue(true);
-//    }
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
