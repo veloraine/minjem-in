01cdf77088a27bb709c5156c9063926a35e401e9
Brandon Ivander <brandon.ivander@ui.ac.id>
2022-04-25 23:21:21 +0700

    Bug error auth

--- src/test/java/id/ac/ui/cs/advprog/minjemin/item/model/ItemDTOTest.java
+++ src/test/java/id/ac/ui/cs/advprog/minjemin/item/model/ItemDTOTest.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.minjemin.item.model;
 
-import id.ac.ui.cs.advprog.minjemin.item.model.ItemDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
