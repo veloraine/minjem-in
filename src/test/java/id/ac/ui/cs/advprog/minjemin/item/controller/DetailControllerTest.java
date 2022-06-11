7caade9039f1a4352edb4e05caaa87f4416f2015
Stephanus Dario Kohar <stephanus.dario@ui.ac.id>
2022-06-11 21:42:30 +0700

    refactor controller for detail user

---
+++ src/test/java/id/ac/ui/cs/advprog/minjemin/item/controller/DetailControllerTest.java
@@ -0,0 +1,60 @@
+package id.ac.ui.cs.advprog.minjemin.item.controller;
+
+import id.ac.ui.cs.advprog.minjemin.auth.service.MinjeminUserDetailsService;
+import id.ac.ui.cs.advprog.minjemin.auth.service.SecurityService;
+import id.ac.ui.cs.advprog.minjemin.item.model.ItemDTO;
+import id.ac.ui.cs.advprog.minjemin.item.service.ItemService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(controllers = DetailController.class)
+public class DetailControllerTest {
+
+    private ItemDTO itemDTO;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ItemService itemService;
+
+    @MockBean
+    private SecurityService securityService;
+
+    @MockBean
+    private MinjeminUserDetailsService minjeminUserDetailsService;
+
+    @BeforeEach
+    void setUp() {
+        itemDTO = ItemDTO.builder()
+                .name("scouter")
+                .desc("ini scouter")
+                .harga(9000)
+                .status("tersedia")
+                .base64Image("aaa")
+                .build();
+        itemDTO.setId("item-1");
+    }
+
+    @Test
+    void whenGetItemDetailReturnStatus200() throws Exception {
+        when(itemService.getItemObject("item-1")).thenReturn(itemDTO);
+
+        mockMvc.perform(get("/items/detail/item-1"))
+                .andExpect(status().isOk())
+                .andExpect(handler().methodName("itemDetail"))
+                .andExpect(model().attributeDoesNotExist())
+                .andExpect(view().name("items/detail"));
+    }
+}
